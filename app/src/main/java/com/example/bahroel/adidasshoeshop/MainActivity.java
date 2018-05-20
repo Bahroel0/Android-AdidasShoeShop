cb9cb0a22bcf2d940db220d603de510cfe07ccab
aulaf <alfienhaq@gmail.com>
2018-05-21 03:38:23 +0700

    update library

--- app/src/main/java/com/example/bahroel/adidasshoeshop/MainActivity.java
+++ app/src/main/java/com/example/bahroel/adidasshoeshop/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.bahroel.adidasshoeshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewFlipper viewFlipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int images[] = {R.drawable.flip1,R.drawable.flip2,R.drawable.flip3};
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewflipper1);
+        for (int image:images){
+            flipperImage(image);
+        }
+    }
+    private void flipperImage(int image) {
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setInAnimation(this,android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this,android.R.anim.slide_out_right);
     }
 }
