dc8d5236fcccab0b9c0d55de7020764add762e3f
Bahroel0 <bahrul123amaruddin@gmail.com>
2018-05-20 16:30:17 +0700

    fixing menu toolbar

---
+++ app/src/main/java/com/example/bahroel/adidasshoeshop/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.bahroel.adidasshoeshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN, android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                android.content.Intent i = new android.content.Intent(SplashScreen.this, MainActivity.class);
+                i.addFlags(android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+    }
+}
