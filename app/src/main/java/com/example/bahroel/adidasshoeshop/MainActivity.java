9b39eba713cd3c53221d4a0045b5e7d5e4d13a70
aulaf <alfienhaq@gmail.com>
2018-05-24 14:27:07 +0700

    rating sudah tp belum sempurna

--- app/src/main/java/com/example/bahroel/adidasshoeshop/MainActivity.java
+++ app/src/main/java/com/example/bahroel/adidasshoeshop/MainActivity.java
@@ -3,16 +3,23 @@ package com.example.bahroel.adidasshoeshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
     ViewFlipper viewFlipper;
+    ScrollView scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int images[] = {R.drawable.flip1,R.drawable.flip2,R.drawable.flip3};
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
+        RelativeLayout.LayoutParams parameter =  (RelativeLayout.LayoutParams) scrollView.getLayoutParams();
+        parameter.setMargins(0, 0, 0, 30); // left, top, right, bottom
+        scrollView.setLayoutParams(parameter);
         viewFlipper = (ViewFlipper)findViewById(R.id.viewflipper1);
         for (int image:images){
             flipperImage(image);
