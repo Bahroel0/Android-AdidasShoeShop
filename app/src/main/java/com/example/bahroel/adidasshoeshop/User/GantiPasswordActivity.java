df4c2fb6eb5a671942a90a2e31b5fbd0e8f940b9
Bahroel0 <bahrul123amaruddin@gmail.com>
2018-06-03 03:29:18 +0700

    add deskripsi xml

---
+++ app/src/main/java/com/example/bahroel/adidasshoeshop/User/GantiPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.example.bahroel.adidasshoeshop.User;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bahroel.adidasshoeshop.R;
+
+public class GantiPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ganti_password);
+    }
+}
